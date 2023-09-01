981ffab3c77def6062c9d9180df02e49aa39cf1b
douglasdago <134888368+douglasdago@users.noreply.github.com>
2023-09-01 10:17:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,33 @@
 import streamlit as st
 import pandas as pd
 import nunpy as sp
+import http.client
+
+conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+headers = {
+    'x-rapidapi-host': "v3.football.api-sports.io",
+    'x-rapidapi-key': "XxXxXxXxXxXxXxXxXxXxXxXx"
+    }
+
+conn.request("GET", "/{endpoint}", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
+
+
+
+
+
+
+
+st.title("SoccerPrev")
+
+st.sidebar.header("Leagues")
+
+st.sidebar.header("Season")
+
+
 
