fad4f75b14144487c81a9b048ae41bf4baff468c
douglasdago <134888368+douglasdago@users.noreply.github.com>
2023-09-02 11:07:31 -0300

    Update appi.py

--- Data/appi.py
+++ Data/appi.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Soccer Back")
+
+st.sidebar.header("Season")
+selected_season = st.sidebar.selectbox('Season', ['2022/2023','2021/2022','2020/2021'])
+
+# Webscraping football data
+
+def load-data(season):
+    url = "https://www.football-data.co.uk/mmz4281/2324/"+season+".xlsx"
+    data = pd.read_xlsx(url)
+    return data
+
+df = load_data(selected_season)
+
 
